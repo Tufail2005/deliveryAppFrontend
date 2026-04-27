0a480f1eaeb3d4ac4e6156b668fd17cfa7ec95e7
tufail2005 <tufail1526@gmail.com>
2026-04-27 22:32:07 +0530

    changed in main layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,19 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return <Stack screenOptions={{headerShown: false}}/>;
-// }
-
-// app/_layout.tsx
-
-// 1. IMPORT YOUR GLOBAL CSS HERE!
-// (Adjust the path if your global.css is in a different folder, like '../src/global.css')
+import { Stack } from "expo-router";
+import { styled } from "nativewind";
+import { SafeAreaProvider as RNSafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
-import { Stack } from "expo-router";
+const SafeAreaProvider = styled(RNSafeAreaView);
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      {/* We add this to hide the header for our new customer screens */}
-      <Stack.Screen name="(customer)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider className="flex-1 bg-background pt-20">
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* We add this to hide the header for our new customer screens */}
+        <Stack.Screen name="(customer)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
