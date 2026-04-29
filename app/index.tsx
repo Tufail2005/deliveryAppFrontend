6534f8f3d61f7cffd8d26d2a5706752438179038
ashisdutta <ashisdutta795@gmail.com>
2026-04-29 21:08:53 +0530

    edited index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,3 @@
-// import "@/global.css";
-// import { Link } from "expo-router";
-// import { Text, View } from "react-native";
-
-// export default function App() {
-//   return (
-//     <View className="flex-1 items-center justify-center bg-white">
-//       <Text className="text-xl font-bold color-primary">
-//         Welcome to Nativewind!
-//       </Text>
-
-//       <Link href="/(auth)/login" className="mt-4 rounded bg-primary text-white p-4 ">login</Link>
-
-//       <Link href="/(auth)/verify" className="mt-4 rounded bg-primary text-white p-4 ">verify</Link>
-//     </View>
-//   );
-// }
-
 import { useRouter } from "expo-router";
 import React from "react";
 import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
